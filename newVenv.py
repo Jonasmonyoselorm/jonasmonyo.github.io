012be01ef131a0867faba27a077041aeacdc1598
Jonas Monyo <jonasmonyo@gmail.com>
2022-09-03 21:46:31 +0000

    save

---
+++ newVenv.py
@@ -0,0 +1,7 @@
+# CREATING Virtual Environment in Python
+
+$ python -m venv virt	#this creates virtual environment call virt
+$ source virt/Scripts/activate	#to activate the virtual envrionment
+$ deactivate #to disable the virtual environment
+
+
