54755f6a392f6fb914752667829af54c732fa24f
Jonas Monyo <jonasmonyo@gmail.com>
2022-09-04 00:35:24 +0000

    saving all new projects

--- myclub/event/urls.py
+++ myclub/event/urls.py
@@ -4,4 +4,9 @@ from . import views
 app_name = "event"
 urlpatterns = [
     path('', views.index, name="index"),
+
+    # using path convertors like 
+    # "int: numbers, str: strings, path: whole urls/, 
+    # slug: hyphen- and _underscores, UUID: universally unique identifier"***
+    path('<int:year>/<str:month>/', views.index, name="index"), 
 ]
--- myclub/event/views.py
+++ myclub/event/views.py
@@ -1,7 +1,42 @@
+from re import M
 from django.shortcuts import render
+
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
 
 # Create your views here.
 
 
-def index(request):
-    return render(request, 'index.html', {})
+def index(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
+    name = "Monyo"
+
+    month = month.capitalize() # convert case to initial case letters
+
+    # Convert month name to number
+    month_number = list(calendar.month_name).index(month)
+    month_number = int(month_number)
+
+    # create a calendar
+    cal = HTMLCalendar().formatmonth(
+        year,
+        month_number
+    )
+
+    # Get current year and date
+    timeNow = datetime.now()
+    current_year = timeNow.year
+    
+    # Get current time
+
+    time = timeNow.strftime('%D %I:%M:%S %A') # READ datetime @ gooogle, python datetime
+
+    return render(request, 'index.html', {
+        "last_name" : name,
+        "year": year,
+        "month": month,
+        "mon_number": month_number, 
+        "calender": cal,
+        "currentYear": current_year,
+        "currentTime": time,
+    })
